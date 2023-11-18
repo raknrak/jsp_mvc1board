bc30504b7cdb987f53910a846370d238cb6c7b60
raknrak <140072536+raknrak@users.noreply.github.com>
2023-11-18 22:47:01 +0900

    쿼리문 띄어쓰기 수정

--- java/model1/board/BoardDAO.java
+++ java/model1/board/BoardDAO.java
@@ -20,7 +20,7 @@ public class BoardDAO  extends JDBConnect {
         // 게시물 수를 얻어오는 쿼리문 작성
         String query = "SELECT COUNT (*) FROM board";
         if (map.get("searchWord") != null) {
-            query += "WHERE" + map.get("searchField") + " "
+            query += " WHERE" + map.get("searchField") + " "
                     + "LIKE '%" + map.get("searchWord") + "%'";
         }
         try {
